4a18cd3dade4fdd416ea5d2b2903a07875b18b9f
yukika-0114 <liuzouxiaoxue@gmail.com>
2026-08-27 12:26:41 +0900

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -10,4 +10,11 @@ if (!supabaseUrl || !supabaseAnonKey) {
   );
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: false,
+    storage: window.localStorage,
+  },
+});
